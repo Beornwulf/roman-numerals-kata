9ca1fae04640a9b71ff1801cfb0d76a6acc5763e
Kay Sharrock <sharrock.kay@gmail.com>
2022-12-22 13:30:37 +0000

    numerals up to M added, untested but not breaking

--- src/numerals.js
+++ src/numerals.js
@@ -21,6 +21,12 @@ function arabicToRoman(number) {
         return false;
     }
     const numerals = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
         "L": 50,
         "XL": 40,
         "X": 10,
